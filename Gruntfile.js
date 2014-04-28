dd2554210f202ea788469865e23803116c220425
Nick Holub <nikholub@gmail.com>
2014-04-28 16:14:12 -0700

    JS/CSS dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,7 +60,7 @@ module.exports = function (grunt) {
         port: 9001,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: 'localhost',
-        livereload: 35729
+        livereload: 35739
       },
       livereload: {
         options: {
